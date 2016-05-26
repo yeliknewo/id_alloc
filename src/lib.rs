bfa68d9cdcca4927f3956dc800a9811e6cc09a36
Kiley Owen <yelikdev@gmail.com>
2016-05-25 20:31:30 -0400

    Broken

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#[test]
+fn alloc_1() {
+    let mut node = Node::new_all();
+    assert!(node.alloc() == Some(0));
+}
+
+#[test]
+fn alloc_2() {
+    let mut node = Node::new_all();
+    assert!(node.alloc() == Some(0));
+    assert!(node.alloc() == Some(1));
+}
+
+#[test]
+fn alloc_3() {
+    let mut node = Node::new_all();
+    assert!(node.alloc() == Some(0));
+    assert!(node.alloc() == Some(1));
+    assert!(node.alloc() == Some(2));
+}
+
+#[test]
+fn dealloc_1() {
+    let mut node = Node::new_all();
+    assert!(node.alloc() == Some(0));
+    node.dealloc(0);
+    assert!(node.alloc() == Some(0));
 }
 
 type Id = u64;
@@ -17,6 +39,15 @@ struct Node {
 }
 
 impl Node {
+    fn new(id: Id) -> Node {
+        Node {
+            bot: id,
+            top: id,
+            left: None,
+            right: None,
+        }
+    }
+
     fn new_all() -> Node {
         Node {
             bot: Id::min_value(),
@@ -24,6 +55,59 @@ impl Node {
 
             left: None,
             right: None,
+        }
+    }
+
+    fn alloc(&mut self) -> Option<Id> {
+        if let Some(lonely) = self.find_lonely() {
+            Some(lonely)
+        } else if let Some(leaf) = self.find_leaf(true) {
+            Some(leaf)
+        } else {
+            None
+        }
+    }
+
+    fn dealloc(&mut self, id: Id) {
+        assert!(id < self.bot || id > self.top);
+        if id == self.bot.wrapping_sub(1) {
+            self.bot = self.compact_left(id);
+        } else if id == self.bot.wrapping_add(1) {
+            self.top = self.compact_right(id);
+        } else if id < self.bot {
+            if self.left.is_none() {
+                self.left = Some(Box::new(Node::new(id)));
+            } else {
+                self.left.as_mut().unwrap().dealloc(id);
+            }
+        } else if id > self.top {
+            if self.right.is_none() {
+                self.right = Some(Box::new(Node::new(id)))
+            } else {
+                self.right.as_mut().unwrap().dealloc(id);
+            }
+        } else {
+            panic!("IDK");
+        }
+    }
+
+    fn compact_left(&mut self, id: Id) -> Id {
+        if self.top == id.wrapping_sub(1) {
+            self.bot
+        } else if self.right.is_some() {
+            self.right.as_mut().unwrap().compact_left(id)
+        } else {
+            id
+        }
+    }
+
+    fn compact_right(&mut self, id: Id) -> Id {
+        if self.bot == id.wrapping_add(1) {
+            self.top
+        } else if self.left.is_some() {
+            self.left.as_mut().unwrap().compact_right(id)
+        } else {
+            id
         }
     }
 
@@ -43,46 +127,23 @@ impl Node {
         }
     }
 
-    fn find_leaf(&mut self, parent: Option<Box<Node>>) -> Option<Id> {
+    fn find_leaf(&mut self, right: bool) -> Option<Id> {
         if self.left.is_none() && self.right.is_none() {
-            if parent.is_none() || {
-                match parent {
-                    Some(p) => {
-                        match p.right {
-                            Some(r) => self == r,
-                            None => false,
-                        }
-                    },
-                    None => false,
-                }
-            } {
-
+            if right {
+                let value = self.bot;
+                self.bot += 1;
+                Some(value)
+            } else {
+                let value = self.top;
+                self.top -= 1;
+                Some(value)
             }
+        } else if self.left.is_some() {
+            self.left.as_mut().unwrap().find_leaf(false)
+        } else if self.right.is_some() {
+            self.right.as_mut().unwrap().find_leaf(true)
+        } else {
+            None
         }
-        None
-    }
-}
-
-struct IdManager {
-    node: Node,
-}
-
-impl IdManager {
-    fn new() -> IdManager {
-        IdManager {
-            node: Node::new_all(),
-        }
-    }
-
-    fn alloc(&mut self) -> Option<Id> {
-        let lonely = self.node.find_lonely();
-        if lonely.is_some() {
-            return lonely;
-        }
-        None
-    }
-
-    fn dealloc(&mut self, id: Id) {
-
     }
 }
